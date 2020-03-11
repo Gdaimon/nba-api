0ef389955faa7c2998d081a7c113cb276e60042f
Carlos Andres <darkklitos@gmail.com>
2020-03-11 09:09:56 -0500

    [add] routes aplicacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PlayersComponent }     from './shared/players/players.component';
+import { PlayerComponent }      from './shared/player/player.component';
+import { BuscadorComponent }    from './shared/buscador/buscador.component';
 
 
-const routes: Routes = [];
+const routes : Routes = [
+  { path : 'players', component : PlayersComponent },
+  { path : 'players/:id', component : PlayerComponent },
+  { path : 'buscador/:termino', component : BuscadorComponent },
+  { path : '', redirectTo : 'players', pathMatch : 'full' },
+  { path : '**', component : PlayersComponent },
+];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+@NgModule ( {
+              imports : [RouterModule.forRoot ( routes )],
+              exports : [RouterModule],
+            } )
+export class AppRoutingModule {}
