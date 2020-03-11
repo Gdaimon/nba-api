4bb7b4ca50a9244bd2aae5d463acb38bf193c8f4
Carlos Andres <darkklitos@gmail.com>
2020-03-11 12:08:02 -0500

    [FINAL] download file all players

--- src/app/shared/players/players.component.ts
+++ src/app/shared/players/players.component.ts
@@ -32,4 +32,19 @@ export class PlayersComponent implements OnInit {
   crearJugador() {
     this.router.navigate ( ['players/', 'nuevo'] );
   }
+
+  descargar() {
+    const filename = 'data.json';
+    const jsonStr = JSON.stringify ( this.playersListFilter );
+
+    const element = document.createElement ( 'a' );
+    element.setAttribute ( 'href', 'data:text/plain;charset=utf-8,' + encodeURIComponent ( jsonStr ) );
+    element.setAttribute ( 'download', filename );
+
+    element.style.display = 'none';
+    document.body.appendChild ( element );
+    element.click ();
+
+    document.body.removeChild ( element );
+  }
 }
