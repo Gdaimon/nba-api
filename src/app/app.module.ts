f2e6c73908f5fcb77c5fe959543693c7388ac3c7
Carlos Andres <darkklitos@gmail.com>
2020-03-10 22:14:04 -0500

    [add] componentes de jugadoes y jugador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { PlayersComponent } from './shared/players/players.component';
+import { PlayerComponent } from './shared/player/player.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    PlayersComponent,
+    PlayerComponent
   ],
   imports: [
     BrowserModule,
