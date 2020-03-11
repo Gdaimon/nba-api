f3c170c193157b6a87efa3e7a1bca1ad75b3108f
Carlos Andres <darkklitos@gmail.com>
2020-03-10 23:52:40 -0500

    [add] metodes de consulta todos y unico

--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Player }     from '../models/player';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable ( {
+                providedIn : 'root',
+              } )
 export class PlayerService {
+  URL_API : string = 'https://www.balldontlie.io/api/v1/players';
 
-  constructor() { }
+
+  constructor( private httpClient : HttpClient ) {
+  }
+
+  getAllPlayers() : Observable<Player> {
+    return this.httpClient.get<Player> ( this.URL_API );
+  }
+
+  getPlayer( id : string ) : Observable<Player> {
+    return this.httpClient.get<Player> ( `${ this.URL_API }/${ id }` );
+  }
 }
