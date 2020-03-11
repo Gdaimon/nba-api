5bc249249e5c37340a78e5007f86189e90e2fa7a
Carlos Andres <darkklitos@gmail.com>
2020-03-10 23:48:32 -0500

    [add] Modulo httpClient y componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule }      from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/header/header.component';
+import { AppComponent }     from './app.component';
+import { HeaderComponent }  from './shared/header/header.component';
 import { PlayersComponent } from './shared/players/players.component';
-import { PlayerComponent } from './shared/player/player.component';
+import { PlayerComponent }  from './shared/player/player.component';
+import { HttpClientModule } from '@angular/common/http';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    PlayersComponent,
-    PlayerComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+@NgModule ( {
+              declarations : [
+                AppComponent,
+                HeaderComponent,
+                PlayersComponent,
+                PlayerComponent,
+              ],
+              imports      : [
+                BrowserModule,
+                AppRoutingModule,
+                HttpClientModule,
+              ],
+              providers    : [],
+              bootstrap    : [AppComponent],
+            } )
+export class AppModule {}
