1d2f4f44590d48e18dc10da9bbb9fd48e82fdf0c
Carlos Andres <darkklitos@gmail.com>
2020-03-11 11:52:48 -0500

    [add] manejo de formularios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule }      from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent }     from './app.component';
-import { HeaderComponent }  from './shared/header/header.component';
-import { PlayersComponent } from './shared/players/players.component';
-import { PlayerComponent }  from './shared/player/player.component';
-import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule }  from './app-routing.module';
+import { AppComponent }      from './app.component';
+import { HeaderComponent }   from './shared/header/header.component';
+import { PlayersComponent }  from './shared/players/players.component';
+import { PlayerComponent }   from './shared/player/player.component';
+import { HttpClientModule }  from '@angular/common/http';
 import { BuscadorComponent } from './shared/buscador/buscador.component';
+import { FormsModule }       from '@angular/forms';
 
 @NgModule ( {
               declarations : [
@@ -21,6 +22,7 @@ import { BuscadorComponent } from './shared/buscador/buscador.component';
                 BrowserModule,
                 AppRoutingModule,
                 HttpClientModule,
+                FormsModule,
               ],
               providers    : [],
               bootstrap    : [AppComponent],
