9644767594bb2b425ee6f13e31809729d7f2ba50
Carlos Andres <darkklitos@gmail.com>
2020-03-11 09:11:17 -0500

    [update] informacion player

--- src/app/shared/player/player.component.ts
+++ src/app/shared/player/player.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Player }            from '../../models/player';
+import { ActivatedRoute }    from '@angular/router';
+import { PlayerService }     from '../../services/player.service';
 
-@Component({
-  selector: 'app-player',
-  templateUrl: './player.component.html',
-  styleUrls: ['./player.component.scss']
-})
+@Component ( {
+               selector    : 'app-player',
+               templateUrl : './player.component.html',
+               styleUrls   : ['./player.component.scss'],
+             } )
 export class PlayerComponent implements OnInit {
 
-  constructor() { }
+  player : Player;
 
-  ngOnInit(): void {
+  constructor( private activateRoute : ActivatedRoute,
+               private playerService : PlayerService ) {
+  }
+
+  ngOnInit() : void {
+    this.activateRoute.params.subscribe ( params => {
+      const { id } = params;
+      this.playerService.getPlayer ( id )
+          .subscribe ( ( response : any ) => {
+            this.player = response;
+            console.log ( this.player );
+          } );
+
+    } );
   }
 
 }
