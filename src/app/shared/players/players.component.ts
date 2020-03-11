23f98e4039fd17e36eb03c9d6e3d9b1d12868305
Carlos Andres <darkklitos@gmail.com>
2020-03-10 23:53:11 -0500

    [update] vista jugadores

--- src/app/shared/players/players.component.ts
+++ src/app/shared/players/players.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Player }            from '../../models/player';
+import { PlayerService }     from '../../services/player.service';
+import { Router }            from '@angular/router';
 
-@Component({
-  selector: 'app-players',
-  templateUrl: './players.component.html',
-  styleUrls: ['./players.component.scss']
-})
+@Component ( {
+               selector    : 'app-players',
+               templateUrl : './players.component.html',
+               styleUrls   : ['./players.component.scss'],
+             } )
 export class PlayersComponent implements OnInit {
 
-  constructor() { }
+  playersList : Array<Player> = [];
+  playersListFilter : Array<Player> = [];
 
-  ngOnInit(): void {
+
+  constructor( public playerService : PlayerService,
+               public router : Router,
+  ) {
   }
 
+  ngOnInit() : void {
+    this.playerService.getAllPlayers ()
+        .subscribe ( ( response : any ) => {
+          this.playersList = response.data;
+          this.playersListFilter = [... this.playersList];
+        } );
+  }
+
+  detailPlayer( player : Player ) {
+    console.dir ( player );
+    this.router.navigate ( ['players/', player.id] );
+  }
 }
