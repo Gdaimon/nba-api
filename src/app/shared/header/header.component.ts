40e0472126c35bf86f82a3c90c481b8bb9b6eaff
Carlos Andres <darkklitos@gmail.com>
2020-03-11 09:10:47 -0500

    [add] ontener parametro de busqueda

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Router }            from '@angular/router';
 
-@Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
-})
+@Component ( {
+               selector    : 'app-header',
+               templateUrl : './header.component.html',
+               styleUrls   : ['./header.component.scss'],
+             } )
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor( private router : Router ) {
   }
 
+  ngOnInit() : void {
+  }
+
+  buscador( texto : string, event ) {
+    event.preventDefault ();
+    texto = texto.trim ();
+    if ( texto.length > 0 ) {
+      this.router.navigate ( ['/buscador', texto] );
+    } else {
+      this.router.navigate ( ['/players'] );
+    }
+  }
 }
